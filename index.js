b45e02329eb50c1b078e3cc82eae7778357005df
naykiex <naykiex@gmail.com>
2024-02-12 17:41:58 +0700

    2/12

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const axios = require('axios')
+const path = require("path")
 const app = express()
 var bodyParser = require('body-parser')
 
 
-const base_url = "http://localhost:3000"
+//const base_url = "http://localhost:3000"
+const base_url = "node57200-punnatheenoderest.proen.app.ruk-com.cloud"
 
+app.set("views", path.join(__dirname, "/public/views"))
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
